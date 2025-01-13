04dbd2e491ab977ec719f9031a2b788edcac4fae
g <g@g>
2025-01-13 19:18:20 +0100

    wip

--- zmirror.py
+++ zmirror.py
@@ -19,16 +19,16 @@ import os
 log = logging.getLogger(__name__)
 
 def convert_dict_to_strutex(dictionary):
-    result_string = "env"
     for key, value in dictionary.items():
         result_string = result_string + f"\n\t{key}:: {value}"
     return result_string
 
 def main():
-    logfile = '/var/run/zmirror/log.st'
-    parent_directory = os.path.dirname(logfile)
+    logfile_path = '/var/run/zmirror/log.st'
+    config_file_path = "/etc/zmirror/config.yml"
+    parent_directory = os.path.dirname(logfile_path)
     os.makedirs(parent_directory, exist_ok = True)
-    logging.basicConfig(filename=logfile, 
+    logging.basicConfig(filename=logfile_path, 
                         level=logging.INFO, 
                         format='%(asctime)s %(levelname)-8s %(message)s',    
                         datefmt='%Y-%m-%d %H:%M:%S')
@@ -38,100 +38,36 @@ def main():
     for key, value in all_env_vars.items():
         if key[0] != "_":
             new_all_env_vars[key] = value
-    log.info(f"environment variables: {convert_dict_to_strutex(new_all_env_vars)}")
-    with open('filename.yaml') as f:
-        my_dict = yaml.safe_load(f)
+    with open(config_file_path) as config_file:
+        config_dict = yaml.safe_load(config_file)
+    if config_dict["log-env"] == "yes":
+        result_string = "env" + convert_dict_to_strutex(new_all_env_vars)
+        log.info(f"environment variables: {result_string}")
+    dm_crypts = config_dict["dm-crypts"]
+    for name, dm_crypt in dm_crypts.items():
+        type = dm_crypt["type"]
+        key_file = dm_crypt["key-file"]
+        if type == "partition":
+            if "zfs" in dm_crypt:
+                zfs = dm_crypt[zfs]
+                pool = zfs["pool"]
+                dev = zfs["dev"]
+                wait_for_scrub_max = zfs["wait-for-scrub-max"]
+                scrub_schedule = zfs["scrub-schedule"]
+                if "on-scrubbed" in zfs:
+                    on_scrubbed = zfs["on-scrubbed"]
+                if "on_resilvered" in zfs:
+                    on_resilvered = zfs["on_resilvered"]
+                
+
+            if "vgs" in dm_crypt:
+                vgs = dm_crypt["vgs"]
+        elif type == "disk":
+            serial = dm_crypt["serial"]
+        
+
+
     
 if __name__ == "__main__":
     main()
 
-
-"""
-
-# use the binary from the env variable ZPOOL when available or use zpool from PATH (which is available in udev)
-
-udev block
-    remove
-        disk
-            match
-                DEVTYPE: disk
-
-                ID_USB_VENDOR_ID
-                ID_USB_MODEL_ID
-                ID_PART_TABLE_UUID
-            
-            reaction
-                partprobe???
-
-        partition
-            match
-                DEVTYPE: partition
-                PARTNAME
-                
-                ID_FS_UUID
-                ID_PART_ENTRY_UUID
-
-
-            reaction
-                
-                zpool offline ...
-                # we ignore generated zevents
-
-                cryptsetup close ...
-        virtual disk (decrypted blockdev) or virtual partition
-            match
-                see disk
-            reaction
-                zpool online ...
-    add
-        partition
-            match
-                # ACTION: add
-                DEVTYPE: partition
-
-                PARTNAME: eva-b-main
-
-                ID_FS_UUID: 5734e13f-6732-4c68-bc14-22249afaac17
-
-                # this is the one in /dev/disk/by-partuuid
-                ID_PART_ENTRY_UUID: 57507cd3-dc80-46c5-b93b-ab51dc41ad37
-            
-            reaction
-                
-                cryptsetup open
-                # will trigger next event
-        
-zed
-    # irrelevant, but useful to know how to ignore
-    history
-        ZEVENT_CLASS:: sysevent.fs.zfs.history_event
-        ZEVENT_POOL_GUID:: 0xD842CE6598FB0398
-    # irrelevant
-    scrub start
-        match
-            ZEVENT_CLASS: sysevent.fs.zfs.scrub_start
-    
-    ? scrub completed
-        if configured
-            zpool offline
-            if configured
-                cryptsetup close
-        
-
-
-
-    resilver finished
-        match
-            ZEVENT_CLASS: sysevent.fs.zfs.resilver_finish
-            ZEVENT_SUBCLASS: resilver_finish
-
-        reaction
-            if now falls into scrub interval
-                start scrub
-            else
-                see scrub completed
-            
-
-
-
-"""
