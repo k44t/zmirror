42c3676e558e0894aad6db8f9edda3b7f90e790b
IamK <iamk@k44.at>
2025-09-01 15:00:46 +0200

    updated version number

--- zmirror/defaults.py
+++ zmirror/defaults.py
@@ -4,4 +4,4 @@ ZMIRROR_CONFIG_PATH_DEFAULT = "/etc/zmirror/zmirror-config.yml"
 ZMIRROR_CACHE_PATH_DEFAULT = "/var/lib/zmirror/zmirror-cache.yml"
 ZMIRROR_SOCKET_PATH_DEFAULT = "/run/zmirror/zmirror.socket"
 
-VERSION="0.1.5-alpha"
+VERSION="0.1.6-alpha"
