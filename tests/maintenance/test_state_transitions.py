fb9de0e48bac998555e5133798a6e45bcb54c0f0
devvitheloper <devvitheloper@gmail.com>
2026-04-16 20:37:45 +0200

    split state transition tests from maintenance command tests

---
+++ tests/maintenance/test_state_transitions.py
@@ -0,0 +1,220 @@
+#!/bin/python3
+
+import pytest
+
+from util.util_stage1 import *
+from util.util_stage2 import *
+
+from zmirror.dataclasses import *
+import zmirror.commands
+import zmirror.entities as entities
+
+
+@pytest.fixture(scope="class", autouse=True)
+def setup_before_all_methods():
+  insert_zpool_status_stub()
+  insert_dev_exists_stub()
+  insert_get_zfs_volume_mode_stub()
+  insert_find_provisioning_mode_stub()
+
+
+class Tests():
+
+  @classmethod
+  def setup_class(cls):
+    entities.init_config(config_path="./example-config.yml", cache_path="./tests/commands/res/test_cache.yml")
+
+    for entity in config.cache_dict.values():
+      if type(entity) in {Disk, Partition}:
+        entity.state.what = EntityState.CONNECTED
+      elif type(entity) in {DMCrypt}:
+        entity.state.what = EntityState.INACTIVE
+      else:
+        entity.state.what = EntityState.DISCONNECTED
+
+    zpool = config.config_dict["zpool|name:zmirror-sysfs"]
+    a = config.config_dict["zdev|pool:zmirror-sysfs|name:zmirror-sysfs-a"]
+    cached(zpool).state.what = EntityState.CONNECTED
+    cached(a).state.what = EntityState.CONNECTED
+
+  @classmethod
+  def teardown_class(cls):
+    zmirror.commands.stop_workers()
+
+  def teardown_method(self, method):  # pylint: disable=unused-argument
+    commands.commands = []
+
+  def test_disk_activates_on_child_online(self):
+    disk = config.config_dict["disk|uuid:00000000-0000-0000-0000-000000000004"]
+    partition = config.config_dict["part|name:zmirror-sysfs-s"]
+    disk_cache = cached(disk)
+
+    disk_cache.state.what = EntityState.CONNECTED
+    disk.handle_child_online(partition, EntityState.DISCONNECTED)
+
+    assert disk_cache.state.what == EntityState.ACTIVE
+
+  def test_disk_deactivates_to_connected_on_children_offline(self):
+    disk = config.config_dict["disk|uuid:00000000-0000-0000-0000-000000000004"]
+    partition = config.config_dict["part|name:zmirror-sysfs-s"]
+    disk_cache = cached(disk)
+    partition_cache = cached(partition)
+
+    disk_cache.state.what = EntityState.ACTIVE
+    partition_cache.state.what = EntityState.DISCONNECTED
+    disk.handle_child_offline(partition, EntityState.CONNECTED)
+
+    assert disk_cache.state.what == EntityState.CONNECTED
+
+  def test_disk_onlined_with_online_child_becomes_active(self):
+    disk = config.config_dict["disk|uuid:00000000-0000-0000-0000-000000000004"]
+    partition = config.config_dict["part|name:zmirror-sysfs-s"]
+    disk_cache = cached(disk)
+    partition_cache = cached(partition)
+
+    disk_cache.state.what = EntityState.DISCONNECTED
+    partition_cache.state.what = EntityState.CONNECTED
+    handle_onlined(disk_cache)
+
+    assert disk_cache.state.what == EntityState.ACTIVE
+
+  def test_partition_activates_on_child_online(self):
+    partition = config.config_dict["part|name:zmirror-sysfs-s"]
+    dmcrypt = config.config_dict["crypt|name:zmirror-sysfs-s"]
+    partition_cache = cached(partition)
+
+    partition_cache.state.what = EntityState.CONNECTED
+    partition.handle_child_online(dmcrypt, EntityState.INACTIVE)
+
+    assert partition_cache.state.what == EntityState.ACTIVE
+
+  def test_partition_deactivates_to_connected_on_children_offline(self):
+    partition = config.config_dict["part|name:zmirror-sysfs-s"]
+    dmcrypt = config.config_dict["crypt|name:zmirror-sysfs-s"]
+    partition_cache = cached(partition)
+    dmcrypt_cache = cached(dmcrypt)
+
+    partition_cache.state.what = EntityState.ACTIVE
+    dmcrypt_cache.state.what = EntityState.DISCONNECTED
+    partition.handle_child_offline(dmcrypt, EntityState.CONNECTED)
+
+    assert partition_cache.state.what == EntityState.CONNECTED
+
+  def test_partition_onlined_with_online_child_becomes_active(self):
+    partition = config.config_dict["part|name:zmirror-sysfs-s"]
+    dmcrypt = config.config_dict["crypt|name:zmirror-sysfs-s"]
+    partition_cache = cached(partition)
+    dmcrypt_cache = cached(dmcrypt)
+
+    partition_cache.state.what = EntityState.DISCONNECTED
+    dmcrypt_cache.state.what = EntityState.CONNECTED
+    handle_onlined(partition_cache)
+
+    assert partition_cache.state.what == EntityState.ACTIVE
+
+  def test_partition_load_initial_state_becomes_active_with_online_child(self):
+    partition = config.config_dict["part|name:zmirror-sysfs-s"]
+    dmcrypt = config.config_dict["crypt|name:zmirror-sysfs-s"]
+    dmcrypt_cache = cached(dmcrypt)
+    old_dev_exists = config.dev_exists
+
+    dmcrypt_cache.state.what = EntityState.ACTIVE
+    config.dev_exists = lambda path: path == partition.dev_path()
+    try:
+      assert partition.load_initial_state() == EntityState.ACTIVE
+    finally:
+      config.dev_exists = old_dev_exists
+
+  def test_disk_load_initial_state_becomes_active_with_online_child(self):
+    disk = config.config_dict["disk|uuid:00000000-0000-0000-0000-000000000004"]
+    partition = config.config_dict["part|name:zmirror-sysfs-s"]
+    partition_cache = cached(partition)
+    old_dev_exists = config.dev_exists
+
+    partition_cache.state.what = EntityState.ACTIVE
+    config.dev_exists = lambda path: path == disk.dev_path()
+    try:
+      assert disk.load_initial_state() == EntityState.ACTIVE
+    finally:
+      config.dev_exists = old_dev_exists
+
+  def test_dmcrypt_load_initial_state_becomes_active_with_online_child(self):
+    dmcrypt = config.config_dict["crypt|name:zmirror-sysfs-s"]
+    zdev = config.config_dict["zdev|pool:zmirror-sysfs|name:zmirror-sysfs-s"]
+    zdev_cache = cached(zdev)
+    old_dev_exists = config.dev_exists
+
+    zdev_cache.state.what = EntityState.ACTIVE
+    config.dev_exists = lambda path: path == dmcrypt.dev_path()
+    try:
+      assert dmcrypt.load_initial_state() == EntityState.ACTIVE
+    finally:
+      config.dev_exists = old_dev_exists
+
+  def test_zfs_volume_activates_on_child_online(self):
+    volume = config.config_dict["zvol|pool:zmirror-bak-a|name:sysfs"]
+    zdev = config.config_dict["zdev|pool:zmirror-sysfs|name:zvol/zmirror-bak-a/sysfs"]
+    volume_cache = cached(volume)
+
+    volume_cache.state.what = EntityState.CONNECTED
+    volume.handle_child_online(zdev, EntityState.INACTIVE)
+
+    assert volume_cache.state.what == EntityState.ACTIVE
+
+  def test_zfs_volume_deactivates_to_connected_on_children_offline(self):
+    volume = config.config_dict["zvol|pool:zmirror-bak-a|name:sysfs"]
+    zdev = config.config_dict["zdev|pool:zmirror-sysfs|name:zvol/zmirror-bak-a/sysfs"]
+    volume_cache = cached(volume)
+    zdev_cache = cached(zdev)
+
+    volume_cache.state.what = EntityState.ACTIVE
+    zdev_cache.state.what = EntityState.DISCONNECTED
+    volume.handle_child_offline(zdev, EntityState.CONNECTED)
+
+    assert volume_cache.state.what == EntityState.CONNECTED
+
+  def test_zfs_volume_onlined_with_online_child_becomes_active(self):
+    volume = config.config_dict["zvol|pool:zmirror-bak-a|name:sysfs"]
+    zdev = config.config_dict["zdev|pool:zmirror-sysfs|name:zvol/zmirror-bak-a/sysfs"]
+    volume_cache = cached(volume)
+    zdev_cache = cached(zdev)
+
+    volume_cache.state.what = EntityState.DISCONNECTED
+    zdev_cache.state.what = EntityState.CONNECTED
+    handle_onlined(volume_cache)
+
+    assert volume_cache.state.what == EntityState.ACTIVE
+
+  def test_zfs_volume_update_initial_state_becomes_active_with_online_child(self):
+    volume = config.config_dict["zvol|pool:zmirror-bak-a|name:sysfs"]
+    pool = config.config_dict["zpool|name:zmirror-bak-a"]
+    zdev = config.config_dict["zdev|pool:zmirror-sysfs|name:zvol/zmirror-bak-a/sysfs"]
+    pool_cache = cached(pool)
+    zdev_cache = cached(zdev)
+
+    pool_cache.state.what = EntityState.CONNECTED
+    zdev_cache.state.what = EntityState.ACTIVE
+
+    assert volume.update_initial_state() == EntityState.ACTIVE
+
+  def test_dmcrypt_activates_on_child_online(self):
+    dmcrypt = config.config_dict["crypt|name:zmirror-sysfs-s"]
+    zdev = config.config_dict["zdev|pool:zmirror-sysfs|name:zmirror-sysfs-s"]
+    dmcrypt_cache = cached(dmcrypt)
+
+    dmcrypt_cache.state.what = EntityState.CONNECTED
+    dmcrypt.handle_child_online(zdev, EntityState.INACTIVE)
+
+    assert dmcrypt_cache.state.what == EntityState.ACTIVE
+
+  def test_dmcrypt_deactivates_to_connected_on_children_offline(self):
+    dmcrypt = config.config_dict["crypt|name:zmirror-sysfs-s"]
+    zdev = config.config_dict["zdev|pool:zmirror-sysfs|name:zmirror-sysfs-s"]
+    dmcrypt_cache = cached(dmcrypt)
+    zdev_cache = cached(zdev)
+
+    dmcrypt_cache.state.what = EntityState.ACTIVE
+    zdev_cache.state.what = EntityState.DISCONNECTED
+    dmcrypt.handle_child_offline(zdev, EntityState.CONNECTED)
+
+    assert dmcrypt_cache.state.what == EntityState.CONNECTED
---
+++ tests/test_yaml_tags.py
@@ -0,0 +1,69 @@
+import yaml
+import pytest
+
+from kpyutils.kiify import yaml_data
+from zmirror.dataclasses import ZMirror, ZPool, ZDev, Disk, Partition, DMCrypt, ZFSVolume
+
+
+def test_yaml_data_name_alias_uses_only_preferred_tag_by_default():
+  @yaml_data(name="widget")
+  class Widget:
+    value: int = 0
+
+  parsed_alias = yaml.full_load("!widget {value: 7}")
+
+  assert isinstance(parsed_alias, Widget)
+  assert parsed_alias.value == 7
+
+  with pytest.raises(yaml.constructor.ConstructorError):
+    yaml.full_load("!Widget {value: 9}")
+
+
+def test_yaml_data_name_alias_can_enable_legacy_class_name_tag():
+  @yaml_data(name="gadget", also_use_class_name=True)
+  class Gadget:
+    value: int = 0
+
+  parsed_alias = yaml.full_load("!gadget {value: 7}")
+  parsed_legacy = yaml.full_load("!Gadget {value: 9}")
+
+  assert isinstance(parsed_alias, Gadget)
+  assert isinstance(parsed_legacy, Gadget)
+  assert parsed_alias.value == 7
+  assert parsed_legacy.value == 9
+
+
+def test_zmirror_short_yaml_tags_are_recognized():
+  text = """
+--- !zmirror
+content:
+  - !zpool
+    name: tank
+    content:
+      - !zdev
+        pool: tank
+        name: d1
+      - !zvol
+        name: data
+      - !part
+        name: p1
+      - !disk
+        uuid: 11111111-1111-1111-1111-111111111111
+      - !crypt
+        name: c1
+"""
+
+  config = yaml.full_load(text)
+
+  assert isinstance(config, ZMirror)
+  assert isinstance(config.content[0], ZPool)
+  assert isinstance(config.content[0].content[0], ZDev)
+  assert isinstance(config.content[0].content[1], ZFSVolume)
+  assert isinstance(config.content[0].content[2], Partition)
+  assert isinstance(config.content[0].content[3], Disk)
+  assert isinstance(config.content[0].content[4], DMCrypt)
+
+
+def test_zmirror_does_not_accept_class_name_yaml_tags_for_short_named_types():
+  with pytest.raises(yaml.constructor.ConstructorError):
+    yaml.full_load("--- !ZMirror {}")
